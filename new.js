535ce4fd47cd7f4129b8cc30b90b03026b1d2f92
Archa lal s <archa0002@gmail.com>
2025-07-12 22:54:28 +0530

    more questions

--- new.js
+++ new.js
@@ -36,9 +36,51 @@ console.log(newArr);
 //Find the second largest number in an array
 for(i in array){
 
-    let 
+    // let 
 
 }
+//reverse an array
+let arr=[3,4,5,6]
+let revArray=[]
+for(i=arr.length-1;i>=0;i--){
+    revArray.push(arr[i])
+
+
+}
+console.log(revArray);
+console.log(arr.reverse());
+
+//reverse an obj
+
+let word="archa"
+let reversedObj=""
+for(i of word){
+    reversedObj=i+reversedObj
+
+}
+console.log(reversedObj);
+console.log(word.split('').reverse().join(""));
+
+//// Remove duplicates from an array.
+let arr1=[1,2,3,4,4,5]
+let duplicate=[]
+for(i=0;i<=arr1.length-1;i++){
+    if(!duplicate.includes(arr1[i])){
+        duplicate.push(arr1[i])
+    }
+    
+}
+console.log(duplicate);
+
+//remove duplicate from an object
+
+//second largest number
+
+let arr2=[1,2,3,4,5,8]
+let sorted=arr2.sort((a,b)=>b-a)
+console.log(sorted[1]);
+
+
 
 
 
